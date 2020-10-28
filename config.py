693d72ddfe332505f796b47d1f58422eebf4099a
Trevor White <trevor.white@wayne.edu>
2020-10-28 16:02:54 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+"""
+Replace the placeholders below with your info inside of the ticks.
+
+Currently supported carriers:
+  att
+  tmobile
+  verizon
+  sprint
+  virgin
+  boost
+  cricket
+  metro
+  us cellular
+  xfinity
+"""
+
+number = '0000000000'
+carrier = 'att'
+email = 'youremail@gmail.com'
+password = 'youremailpassword'
